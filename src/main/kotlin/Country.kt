2b398cc1a8fe5de6883081158cf785ff74d7216a
Alex Kim <alexkjh2011@gmail.com>
2023-07-05 00:10:12 +0900

    Create Country.kt

---
+++ src/main/kotlin/Country.kt
@@ -0,0 +1,14 @@
+fun handleCountry(country: Country) {
+    when (country) {
+        Country.AMERICA -> TODO()
+        Country.KOREA -> TODO()
+    }
+}
+
+
+enum class Country(
+    private val code: String,) {
+
+    KOREA("KO"),
+    AMERICA("US")
+}
